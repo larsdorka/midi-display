1afac021dd687606cc6027b1f7515cd4f037d58e
larsdorka <lars.dorka@googlemail.com>
2017-06-29 22:08:02 +0200

    Refactor midi input handling in class midiInput

--- midi-display/midi-display.py
+++ midi-display/midi-display.py
@@ -2,6 +2,8 @@ import pygame, sys, time
 import pygame.midi
 from pygame.locals import *
 
+import midiInput
+
 #configuration constants
 FULLSCREEN = False
 
@@ -18,74 +20,14 @@ def init():
     DISPLAYWIDTH = DISPLAYSURFACE.get_width()
     DISPLAYHEIGHT = DISPLAYSURFACE.get_height()
     BIGFONT = pygame.font.Font('freesansbold.ttf', DISPLAYHEIGHT // 5)
-    print(pygame.font.get_default_font())
-    #initialize midi
-    global MIDIDATA, MIDIACTIVE, MIDIDEVICE
-    pygame.midi.init()
-    midiDeviceCount = pygame.midi.get_count()
-    #for i in range(midiDeviceCount):
-    #        print (pygame.midi.get_device_info(i))
-    defaultInputId = pygame.midi.get_default_input_id()
-    if defaultInputId >= 0:
-        print (pygame.midi.get_device_info(defaultInputId))
-        MIDIDEVICE = pygame.midi.Input(defaultInputId)
-        MIDIACTIVE = True
-    else:
-        print("ERROR: no midi device found!")
-        MIDIACTIVE = False
-    MIDIDATA = []
-    for index in range(128):
-        MIDIDATA.append(0)
-    #MIDIDATA[127] = 63
-        
-
-#main application loop
-def main():
-    #loopCounter = 0
-    old_number = 0
-    new_number = 0
-    while True:
-        time.sleep(0.1)
-        checkForExit()
-        if MIDIACTIVE:
-            readMidiInput()
-        new_number = calcNumber()
-        if new_number != old_number:
-            DISPLAYSURFACE.fill((0,0,0))
-            if new_number != 0:
-                #displayText = BIGFONT.render(str(loopCounter).zfill(5), True, (255, 255, 255), (0, 0, 0))
-                displayText = BIGFONT.render(str(new_number).zfill(5), True, calcColor(), (0, 0, 0))
-                displayRect = displayText.get_rect()
-                displayRect.center = (DISPLAYWIDTH // 2, DISPLAYHEIGHT // 2)
-                DISPLAYSURFACE.blit(displayText, displayRect)
-            old_number = new_number
-        pygame.display.update()
-        #loopCounter += 1
-
-
-#read midi input
-def readMidiInput():
-    midiInputData = []
-    while MIDIDEVICE.poll():
-        midiInputData.append(MIDIDEVICE.read(1))
-    if midiInputData != []:
-        #print (midiInputData)
-        for message in midiInputData:
-            print (message)
-            if len(message) > 0:
-                if len(message[0]) > 0:
-                    status = message[0][0][0] & 240
-                    key = message[0][0][1]
-                    velocity = message[0][0][2]
-                    print ("status: {}, key: {}, velocity: {}".format(status, key, velocity))
-                    MIDIDATA[key] = velocity
+    #print(pygame.font.get_default_font())
 
 
 #calculate 'the number'
 def calcNumber():
     number = 0
     for index in range(128):
-        if MIDIDATA[index] > 0:
+        if midi.midiData[index] > 0:
             number += 2 ** (index % 16)
     number %= 100000
     return number
@@ -96,9 +38,9 @@ def calcColor():
     color = 0
     keyCounter = 0
     for index in range(128):
-        if MIDIDATA[index] > 0:
+        if midi.midiData[index] > 0:
             keyCounter += 1
-            color += MIDIDATA[index] * 2
+            color += midi.midiData[index] * 2
     print (keyCounter)
     if keyCounter == 0:
         return (0, 0, 0)
@@ -119,5 +61,26 @@ def checkForExit():
 #run application loop
 if __name__ == '__main__':
     init()
-    main()
+    midi = midiInput.MidiInput()
+    midi.open()
+    #loopCounter = 0
+    old_number = 0
+    new_number = 0
+    while True:
+        time.sleep(0.1)
+        checkForExit()
+        if midi.connected:
+            midi.readData()
+        new_number = calcNumber()
+        if new_number != old_number:
+            DISPLAYSURFACE.fill((0,0,0))
+            if new_number != 0:
+                #displayText = BIGFONT.render(str(loopCounter).zfill(5), True, (255, 255, 255), (0, 0, 0))
+                displayText = BIGFONT.render(str(new_number).zfill(5), True, calcColor(), (0, 0, 0))
+                displayRect = displayText.get_rect()
+                displayRect.center = (DISPLAYWIDTH // 2, DISPLAYHEIGHT // 2)
+                DISPLAYSURFACE.blit(displayText, displayRect)
+            old_number = new_number
+        pygame.display.update()
+        #loopCounter += 1
     
---
+++ midi-display/midiInput.py
@@ -0,0 +1,55 @@
+import pygame
+import pygame.midi
+from pygame.locals import *
+
+class MidiInput:
+    '''class for handling all the midi input data'''
+    
+    def __init__(self):
+        self.midiData = None
+        self.connected = False
+        self.midiDevice = None
+        pygame.midi.init()
+        self.clearData()
+        
+        
+    def open(self, id=-1):
+        if self.midiDevice != None:
+            self.midiDevice.close()
+            self.midiDevice = None
+        if id >= 0:
+            self.midiDevice = pygame.midi.Input(id)
+        else:
+            defaultInputId = pygame.midi.get_default_input_id()
+            if defaultInputId >= 0:   
+                print (pygame.midi.get_device_info(defaultInputId))
+                self.midiDevice = pygame.midi.Input(defaultInputId)
+                self.connected = True
+            else:
+                print("ERROR: no midi default device found!")
+                self.midiDevice = None
+                self.connected = False
+        
+        
+    def clearData(self):
+        self.midiData = []
+        for index in range(128):
+            self.midiData.append(0)
+            
+            
+    def readData(self):
+            midiInputData = []
+            while self.midiDevice.poll():
+                midiInputData.append(self.midiDevice.read(1))
+            if midiInputData != []:
+                for message in midiInputData:
+                    print (message)
+                    if len(message) > 0:
+                        if len(message[0]) > 0:
+                            status = message[0][0][0] & 240
+                            key = message[0][0][1]
+                            velocity = message[0][0][2]
+                            print ("status: {}, key: {}, velocity: {}".format(status, key, velocity))
+                            self.midiData[key] = velocity 
+            
+            
