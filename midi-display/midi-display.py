963eff4b8635bacf4ae4abfc1de2be2f0c611b38
Lars Dorka <lars.dorka@evopro-ag.de>
2017-07-26 14:06:33 +0200

    Add current midi device id to menu

--- midi-display/midi-display.py
+++ midi-display/midi-display.py
@@ -12,7 +12,7 @@ import configuration
 MENU_STRUCTURE = [
     ["main menu",
      "",
-     "1 - select midi input device",
+     "1 - select midi input device: {}",
      "2 - display fullscreen: {}",
      "3 - show debug info: {}",
      "S - save configuration",
@@ -68,13 +68,13 @@ def check_for_input():
     return result
 
 
-def format_main_menu(set_fullscreen, show_debug):
+def format_main_menu(config):
     menu = list()
     menu.append(MENU_STRUCTURE[0][0])
     menu.append(MENU_STRUCTURE[0][1])
-    menu.append(MENU_STRUCTURE[0][2])
-    menu.append(MENU_STRUCTURE[0][3].format(str(set_fullscreen)))
-    menu.append(MENU_STRUCTURE[0][4].format(str(show_debug)))
+    menu.append(MENU_STRUCTURE[0][2].format(str(config.get_config('MIDI_DEVICE_ID'))))
+    menu.append(MENU_STRUCTURE[0][3].format(str(config.get_config('FULL_SCREEN'))))
+    menu.append(MENU_STRUCTURE[0][4].format(str(config.get_config('SHOW_DEBUG'))))
     menu.append(MENU_STRUCTURE[0][5])
     menu.append(MENU_STRUCTURE[0][6])
     menu.append(MENU_STRUCTURE[0][7])
@@ -103,10 +103,10 @@ if __name__ == '__main__':
     display = displayRenderer.DisplayRenderer(debug_log)
     # fullscreen = configuration.get_config('FULL_SCREEN')
     # show_debug = configuration.get_config('SHOW_DEBUG')
-    chord = config.get_config('CHORD')
+    # chord = config.get_config('CHORD')
     show_menu = False
     menu_select = 0
-    menu_page = format_main_menu(config.get_config('FULL_SCREEN'), config.get_config('SHOW_DEBUG'))
+    menu_page = format_main_menu(config)
     display.open(config.get_config('FULL_SCREEN'))
     midi = midiInput.MidiInput(debug_log)
     midi.open(config.get_config('MIDI_DEVICE_ID'))
@@ -122,7 +122,7 @@ if __name__ == '__main__':
                 input_value = ""
                 menu_select = 0
                 show_menu = True
-                menu_page = format_main_menu(config.get_config('FULL_SCREEN'), config.get_config('SHOW_DEBUG'))
+                menu_page = format_main_menu(config)
         if show_menu:
             if menu_select == 0:
                 if input_value == "menu":
@@ -133,10 +133,10 @@ if __name__ == '__main__':
                 elif input_value == "2":
                     config.set_config('FULL_SCREEN', not config.get_config('FULL_SCREEN'))
                     display.open(config.get_config('FULL_SCREEN'))
-                    menu_page = format_main_menu(config.get_config('FULL_SCREEN'), config.get_config('SHOW_DEBUG'))
+                    menu_page = format_main_menu(config)
                 elif input_value == "3":
                     config.set_config('SHOW_DEBUG', not config.get_config('SHOW_DEBUG'))
-                    menu_page = format_main_menu(config.get_config('FULL_SCREEN'), config.get_config('SHOW_DEBUG'))
+                    menu_page = format_main_menu(config)
                 elif input_value == "S":
                     config.save()
                 elif input_value == "X":
@@ -144,7 +144,7 @@ if __name__ == '__main__':
             elif menu_select == 1:
                 if input_value == "menu":
                     menu_select = 0
-                    menu_page = format_main_menu(config.get_config('FULL_SCREEN'), config.get_config('SHOW_DEBUG'))
+                    menu_page = format_main_menu(config)
                 elif input_value in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
                     index = int(input_value)
                     if midi.midi_device_list[index][2] == 1:
@@ -157,7 +157,7 @@ if __name__ == '__main__':
         if midi.connected:
             midi.read_data()
         number = codeGen.calc_number(midi.midi_data)
-        note_name = codeGen.calc_note_name(midi.get_flat_midi_data(), chord)
+        note_name = codeGen.calc_note_name(midi.get_flat_midi_data(), config.get_config('CHORD'))
         color = codeGen.calc_color(midi.midi_data)
         display.render_number(number, color)
         display.render_note_name(note_name, color)
